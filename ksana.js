05903263588b01d2279c2bf0e21cdd73ef84f9c4
willy9826079 <9826079@gmail.com>
2015-02-09 15:39:43 +0800

    first build

---
+++ ksana.js
@@ -0,0 +1,42 @@
+jsonp_handler({
+ "version": "1",
+ "build": 2,
+ "title": "myapp",
+ "minruntime": 1,
+ "baseurl": "http://willy9826079@rawgit.com/willy9826079/myapp/master/",
+ "description": "",
+ "files": [
+  "index.html",
+  "index.css",
+  "bundle.js",
+  "jquery.js",
+  "react-with-addons.js",
+  "ksana.js",
+  "bootstrap.min.js",
+  "bootstrap.min.css",
+  "sample.kdb"
+ ],
+ "filesizes": [
+  552,
+  64,
+  155641,
+  277980,
+  645243,
+  331,
+  35607,
+  113502,
+  97542
+ ],
+ "filedates": [
+  "2015-02-09T07:28:30.777Z",
+  "2015-02-09T07:28:30.776Z",
+  "2015-02-09T07:29:35.114Z",
+  "2015-02-09T07:28:30.780Z",
+  "2015-02-09T07:28:30.785Z",
+  "2015-02-09T07:28:30.780Z",
+  "2015-02-09T07:28:30.775Z",
+  "2015-02-09T07:28:30.773Z",
+  "2015-02-09T07:29:19.913Z"
+ ],
+ "date": "2015-02-09T07:29:36.071Z"
+})
